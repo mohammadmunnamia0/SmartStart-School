a58a024ee4be9712ad01fc689de359facbde5e1d
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-24 10:50:01 +0600

    `Added import of placeholder2 image and updated HeroSection component styles`

--- src/pages/sections/HeroSection.jsx
+++ src/pages/sections/HeroSection.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import placeholder1 from "../../assets/Hero.jpg";
+import placeholder2 from "../../assets/smartstart-logo-side-text-3.png";
 import { PrimaryButton } from "./Buttons";
 
 const HeroSection = () => (
-  <section className="px-4 sm:px-6 md:px-8 lg:px-20 py-8 md:py-12 flex flex-col md:flex-row items-center justify-between gap-8">
+  <section className="px-4 sm:px-6 md:px-8 lg:px-32 py-8 md:py-12 flex flex-col md:flex-row items-center justify-between gap-8">
     <div className="w-full md:max-w-xl text-center md:text-left">
       <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-gray-800">
-        Unlock Your Child's Potential at{" "}
-        <span className="text-orange-500">SmartStart</span>
+        Unlock Your Child's Potential at
       </h1>
+      <img src={placeholder2} alt="" />
       <p className="text-base sm:text-lg text-gray-700 mb-6">
         Our inclusive preschool and elementary program is designed to nurture
         every child's. Schedule a tour to see how we can help
@@ -22,7 +23,7 @@ const HeroSection = () => (
       <img
         src={placeholder1}
         alt="Children playing"
-        className="w-full max-w-sm md:w-80 h-48 sm:h-64 object-cover rounded-3xl shadow-2xl/30 mx-auto"
+        className="lg:w-[70%] object-cover  shadow-[12px_12px_20px_rgba(0,0,0,0.3)] mx-auto"
       />
     </div>
   </section>
